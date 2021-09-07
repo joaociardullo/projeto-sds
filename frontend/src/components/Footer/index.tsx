8e285eff242c27c0a8dbd69716c9b3278575a040
Joao Ciardullo <joaociardullo@hotmail.com>
2021-09-07 20:30:05 -0300

    Basic Static componets

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/joaociardullo/" target="_blank" rel="noreferrer">João Vitor Ciardullo</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/joaociardullo" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
